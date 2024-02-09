8ed69a16e746371084fa2765330c6cf4eaf2bf87
mohamedrhoulam <mohamedrhoulam.55@gmail.com>
2024-02-10 00:20:15 +0100

    Create startup.cs

---
+++ FluentTaskManager.Server/Startup.cs
@@ -0,0 +1,28 @@
+using FluentTaskManager.Server.Data;
+using Microsoft.EntityFrameworkCore;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
