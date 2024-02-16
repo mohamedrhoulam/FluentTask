860836bd83a5de9f0801a06e8014918ff2549728
mohamedrhoulam <mohamedrhoulam.55@gmail.com>
2024-02-16 18:14:56 +0100

    Fix PostTask

--- FluentTaskManager.Server/Models/TaskCreateModel.cs
+++ FluentTaskManager.Server/Models/TaskCreateModel.cs
@@ -1,9 +1,12 @@
+using FluentTaskManager.Server.Models;
+using TaskStatus = FluentTaskManager.Server.Models.TaskStatus;
+
 public class TaskCreateModel
 {
     public string Title { get; set; }
     public string Description { get; set; }
     public int AssignedUserId { get; set; }
     public DateTime DueDate { get; set; }
-    public string Priority { get; set; }
-    public string Status { get; set; }
+    public TaskPriority Priority { get; set; }
+    public TaskStatus Status { get; set; }
 }
